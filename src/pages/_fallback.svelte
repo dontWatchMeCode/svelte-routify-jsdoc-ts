9e5f2d974d731a02b9e408481a6a81057cb863b9
M. Gschwandtner <84477901+dontWatchMeCode@users.noreply.github.com>
2023-07-07 20:43:44 +0200

    format + npm updates

--- src/pages/_fallback.svelte
+++ src/pages/_fallback.svelte
@@ -4,12 +4,12 @@
 
 <div>
     <div>404</div>
-    <br>
+    <br />
+    <div>Page not found.</div>
+    <br />
     <div>
-        Page not found.
-    </div>
-    <br>
-    <div>
-        <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" href={$url('../')}>Go back</a>
+        <a
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            href={$url('../')}>Go back</a>
     </div>
 </div>
